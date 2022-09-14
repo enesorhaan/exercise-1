71c6b7ae831f98ea956cf83f76372afa1bcbf46a
Enes Orhan <enesorhan_1366@hotmail.com>
2022-09-14 14:50:57 +0300

    homework-1.3 created

---
+++ 3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Sorting the words entered by the user from the bottom to the top
+            Console.WriteLine("Enter a positive numbers: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter {0} words: ",n);
+            string[] words = Console.ReadLine().Split(' ');
+
+            Array.Reverse(words);
+
+            foreach (var entry in words)
+            {
+                Console.WriteLine(entry);
+            }
+        }
+    }
+}
