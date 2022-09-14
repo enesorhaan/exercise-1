e49ff0219855a33ac51f69bcbd8d920096531474
Enes Orhan <enesorhan_1366@hotmail.com>
2022-09-14 15:22:01 +0300

    homework-1.4 created

---
+++ 4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //The total number of words and letters in the sentence
+
+            Console.Write("Write a sentence: ");
+            string sentence = Console.ReadLine();
+            string[] words = sentence.Split(" ");
+            
+            Console.WriteLine("Cümledeki toplam kelime sayisi: {0}, toplam harf sayisi: {1}", words.Length, sentence.Length - words.Length + 1);
+        }
+    }
+}
